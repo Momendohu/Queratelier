cf34886ca040c86f3242e9c65393694e147e841b
siroinaba <siroinaba@gmail.com>
2019-01-04 21:30:45 +0900

    プレイヤープレハブ更新（カメラ関係変更） ウェーブ管理クラス追加（作りかけ）

---
+++ Assets/WaveManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+
+    private int NowWave = 0;
+    private float WaveTime = 0;
+
+    public int[] Wave_EnemySum = new int[3];
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        WaveTimer();
+	}
+
+    private void WaveTimer()
+    {
+        WaveTime += Time.deltaTime;
+    }
+
+    public float GetWaveTimer()
+    {
+        return WaveTime;
+    }
+
+    public int GetNowWave()
+    {
+        return NowWave;
+    }
+}
