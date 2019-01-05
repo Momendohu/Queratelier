44da492fe80ad2ca14761e08e2486efcef0280d6
阿保達也 <pinog.jh@gmail.com>
2019-01-06 00:39:17 +0900

    beam.cs

---
+++ Assets/Abo/Scripts/Beam.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class Beam : MonoBehaviour {
+    private void OnParticleCollision (GameObject other) {
+        //パーティクルがぶつかったときの処理
+    }
+}
