6256c9f73c5adbe64148fbf11023391561d29f40
阿保達也 <pinog.jh@gmail.com>
2019-01-06 01:25:59 +0900

    bityousei

---
+++ Assets/Abo/Scripts/Beam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class Beam : MonoBehaviour {
+    ParticleSystem ps;
+
+    // これらのリストは、各フレームでトリガーの条件に
+    // 一致するパーティクルを格納します
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+
+    void OnEnable () {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void OnParticleTrigger () {
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter,enter);
+
+        for(int i = 0;i < numEnter;i++) {
+            ParticleSystem.Particle p = enter[i];
+            p.startLifetime = 0;
+            enter[i] = p;
+        }
+
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter,enter);
+    }
+}
