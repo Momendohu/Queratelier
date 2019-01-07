2b94f85970d1e6b468cd589d364298b4c570f193
阿保達也 <pinog.jh@gmail.com>
2019-01-07 16:46:49 +0900

    AudioManager(途中)

---
+++ Assets/Abo/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class AudioManager : SingletonMonoBehaviour<AudioManager> {
+    //=============================================================
+
+
+    //=============================================================
+    private void Init () {
+    }
+
+    private void Awake () {
+        //Instance化をすでにしてるなら
+        if(this != Instance) {
+            Destroy(this);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+        Init();
+    }
+
+    private void Start () {
+
+    }
+
+    private void Update () {
+
+    }
+}
